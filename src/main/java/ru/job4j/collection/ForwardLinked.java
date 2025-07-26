70fcd7dfd0293a2cf6e969d42b56428e862edaf4
JiVM999 <mikhail.zenkov.1994@mail.ru>
2025-07-26 15:52:57 +0300

    2. Создать контейнер на базе связанного списка  [#159 #549166]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,86 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+
+    private int size;
+    private int modCount;
+    private Node<T> head;
+
+    public void add(T value) {
+        if (size == 0) {
+            head = new Node<>(value, null);
+        } else {
+            Node<T> nodeIter = head;
+            for (int i = 0; i < size - 1; i++) {
+                nodeIter = nodeIter.next;
+            }
+            nodeIter.next = new Node<>(value, null);
+        }
+        size++;
+        modCount++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        Node<T> result = head;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.item;
+    }
+
+    public T deleteFirst() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
+        T result = head.item;
+        Node<T> swapNode = head.next;
+        head.item = null;
+        head.next = null;
+        head = swapNode;
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+        return new Iterator<>() {
+
+            int mod = modCount;
+            Node<T> cursor = head;
+
+            @Override
+            public boolean hasNext() {
+                if (mod != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return cursor != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T item = cursor.item;
+                cursor = cursor.next;
+                return item;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        private T item;
+        private Node<T> next;
+
+        Node(T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
